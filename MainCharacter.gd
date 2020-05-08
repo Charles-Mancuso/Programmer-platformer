c8c929e4fd13322791b08ca8fe4d4c8edbc195d8
charles mancuso <charlesmancuso66@yahoo.com>
2020-05-08 10:58:45 -0700

    Add floor tiles and object collision

--- MainCharacter.gd
+++ MainCharacter.gd
@@ -1,20 +1,23 @@
 extends KinematicBody2D
 
+const SPEED = 60
+const GRAVITY = 10
+const JUMP = -250
+const FLOOR = Vector2(0, -1)
+
 var velocity = Vector2()
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 30
+		velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -30
+		velocity.x = -SPEED
 	else:
 		velocity.x = 0
 		
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -30
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 30
-	else:
-		velocity.y = 0
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP
+
+	velocity.y += GRAVITY
 	
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity, FLOOR)
